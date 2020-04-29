9cd0e9692b135d1ef38138f46441046096f4df8f
Nepcam <cameron.nepe@gmail.com>
2020-04-29 17:28:11 +1200

    Add project files.

---
+++ HeadsOrTails/Form1.cs
@@ -0,0 +1,26 @@
+﻿/*
+Create an application with a Show Heads button and a Show Tails button.
+When the user clicks the Show Heads button, an image of a coin should appear.
+When the user clicks the Show Tails button, an image of the Tails side of a coin should appear.
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadsOrTails
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
